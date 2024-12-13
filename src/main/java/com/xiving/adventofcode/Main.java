1a3c4997d2d22e44f01d23956f7ba4ef37d0fd93
Xiving <bf.remmers@gmail.com>
2024-12-13 22:07:26 +0100

    Solution for day 13

--- src/main/java/com/xiving/adventofcode/Main.java
+++ src/main/java/com/xiving/adventofcode/Main.java
@@ -12,7 +12,7 @@ public class Main {
     System.out.println("Solving advent days");
 
     for (AdventDay day : adventDays) {
-      if (day.day != 12) {
+      if (day.day != 13) {
         continue;
       }
 
