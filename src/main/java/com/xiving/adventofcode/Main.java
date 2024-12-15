9e655e44daeb158e98be31dfba24ac1f4548520a
Bart Remmers <bart.remmers@csdigitalmedia.nl>
2024-12-15 15:24:41 +0100

    Solution for day 14

--- src/main/java/com/xiving/adventofcode/Main.java
+++ src/main/java/com/xiving/adventofcode/Main.java
@@ -12,7 +12,7 @@ public class Main {
     System.out.println("Solving advent days");
 
     for (AdventDay day : adventDays) {
-      if (day.day != 13) {
+      if (day.day != 14) {
         continue;
       }
 
