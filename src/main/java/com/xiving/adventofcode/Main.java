7b668499bf0d4058156f2240f406afc67497d4a1
Xiving <bf.remmers@gmail.com>
2024-12-17 20:56:42 +0100

    Solution for day 17

--- src/main/java/com/xiving/adventofcode/Main.java
+++ src/main/java/com/xiving/adventofcode/Main.java
@@ -12,7 +12,7 @@ public class Main {
     System.out.println("Solving advent days");
 
     for (AdventDay day : adventDays) {
-      if (day.day != 16) {
+      if (day.day != 17) {
         continue;
       }
 
