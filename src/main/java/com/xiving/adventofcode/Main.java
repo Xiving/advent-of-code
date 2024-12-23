8ca2d08c8cd141a97ccb305e53519fd85ed89b43
Bart Remmers <bart.remmers@csdigitalmedia.nl>
2024-12-23 22:32:38 +0100

    Solution for day 23

--- src/main/java/com/xiving/adventofcode/Main.java
+++ src/main/java/com/xiving/adventofcode/Main.java
@@ -12,7 +12,7 @@ public class Main {
     System.out.println("Solving advent days");
 
     for (AdventDay day : adventDays) {
-      if (day.day != 22) {
+      if (day.day != 23) {
         continue;
       }
 
