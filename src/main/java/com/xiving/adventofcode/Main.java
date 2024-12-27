e321cf1698cdf619ec983aae67495765abf11c64
Xiving <bf.remmers@gmail.com>
2024-12-27 22:34:02 +0100

    Solution for day 24

--- src/main/java/com/xiving/adventofcode/Main.java
+++ src/main/java/com/xiving/adventofcode/Main.java
@@ -12,7 +12,7 @@ public class Main {
     System.out.println("Solving advent days");
 
     for (AdventDay day : adventDays) {
-      if (day.day != 23) {
+      if (day.day != 24) {
         continue;
       }
 
